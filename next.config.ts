26ad3b6c5ee7f72afe06a19e15c36ee65603b6ee
Aman Sah <amanrck69@gmail.com>
2026-09-13 21:49:09 +0530

    feat(projects): integrate 10 authentic studio projects and real photography from Google Drive

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,16 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '/**',
+      },
     ],
   },
 }
